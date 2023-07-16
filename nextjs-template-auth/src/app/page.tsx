96ed332c9f4cbc6e3ca607478e1a022109d97d94
Alan Tavares <alan1tavares@gmail.com>
2023-07-15 21:35:48 -0300

    feat: adicionado o react-hook-form no projeto
    Alterado o formulário de login para aceitar o react-hookform

--- nextjs-template-auth/src/app/page.tsx
+++ nextjs-template-auth/src/app/page.tsx
@@ -1,27 +1,38 @@
 'use client';
 
 import { Avatar, Button, Container, Flex, FormControl, FormLabel, Input, Text, VStack } from '@chakra-ui/react'
+import { SubmitHandler, useForm } from 'react-hook-form';
+
+interface IFormInput {
+  email: string;
+  senha: string;
+}
+
+const onSubmit: SubmitHandler<IFormInput> = data => console.log(data);
 
 export default function Home() {
+  const {register, handleSubmit} = useForm<IFormInput>();
+
   return (
     <Container as='main'>
       <Flex textAlign={'center'} flexDirection={'column'} gap={6}>
         <Text as='h1' fontSize='6xl'> Bem vindo!</Text>
         <Avatar name='Gato' src='/gato-programando.jpg' alignSelf={'center'} size={'2xl'}/>
-        <form>
+        <form onSubmit={handleSubmit(onSubmit)}>
           <VStack gap={4}>
+
             <FormControl>
               <FormLabel>
                 Email:
               </FormLabel>
-              <Input type='email' />
+              <Input type='email' {...register('email')}/>
             </FormControl>
 
             <FormControl>
               <FormLabel>
                 Senha:
               </FormLabel>
-              <Input type='password' />
+              <Input type='password' {...register('senha')}/>
             </FormControl>
 
             <Button colorScheme='pink' type='submit' width={'100%'}>Login</Button>
